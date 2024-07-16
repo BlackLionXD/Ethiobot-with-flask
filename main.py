e35654471c24b8a97ddead9285751c2faa1f526b
Ibrahim Gidi <114783401+BlackLionXD@users.noreply.github.com>
2024-07-16 10:08:19 +0300

    coros error mitigated

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/')
 
 def home():
     return 'hello, world'
+
+@app.route('/bananas')
+
+def home2():
+    return 'This page has bananas'
+
+@app.route('/bread')
+def home3():
+    return 'This page has bread!'
 
 if __name__ == '__main__':
     app.run(debug=True)
